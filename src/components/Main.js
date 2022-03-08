44c0309f8d9b4f949557332695a81818a75c5287
diegodl2002 <diegodl20022@gmail.com>
2022-03-08 13:36:52 -0300

    add search bar in header plus css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,29 @@
-import { Layout, Input} from 'antd';
+import { Layout, Input, Row, Col} from 'antd';
 const {Header, Footer, Sider, Content } = Layout;
 const {Search } = Input;
 
 const Main = ({ userName }) => {
     return(
         <Layout>
-           <header>
-               encabezado
+           <header className='header'>
+               <Row>
+               <Col xs={{ span: 5}} lg= {{ span: 3}}>Logo</Col>
+               <Col xs={{ span: 19}} lg= {{ span: 16}}>
+                   <div className='header-search'>
+                    <Search
+                    placeholder='¿Que estas buscando?'
+                    onSearch={value => console.log(value)}
+                    enterButton
+                    />
+                   </div>
+                   </Col>
+               <Col xs={{ span: 0}} lg= {{ span: 5}}>Bienvenido {userName}</Col>
+               </Row>
             </header>
-            <Content>
+            <Content className='content'>
                 {userName}
             </Content>
-            <Footer>
+            <Footer className='footer'>
                 Pie
             </Footer>
         </Layout>
