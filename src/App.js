08864592052e717e4035f0b6648d777cf3e47e0b
diegodl2002 <diegodl20022@gmail.com>
2022-03-08 14:50:29 -0300

    create product card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import { useState } from 'react';
 import './App.css';
 import Main from './components/Main'
 
-function App() {
+const PROD = [
+  {
+    id: 'prod1',
+    name: 'notebook',
+    price: 1900
+  },
+  {
+    id: 'prod2',
+    name: 'zapas',
+    price: 5000
+  },
+  {
+    id: 'prod3',
+    name: 'ps4',
+    price: 1900
+  }
+
+]
+ 
+const App= () => {
   const  [ userName, setUserName ] = useState('Diego');
 
   return (
     <div className='App-container'>
-    <Main userName={userName} /> 
+    <Main userName={userName} products={PROD} /> 
     </div>
   );
 }
---
+++ src/components/ProductCard.js
@@ -0,0 +1,13 @@
+
+const ProductCard = ({product}) => {
+    return(
+    <>
+     <div className="product.card">
+      <div>{product.name}</div>
+      <div>{product.price}</div>
+     </div>
+    </>
+    );
+}
+
+export default ProductCard;
